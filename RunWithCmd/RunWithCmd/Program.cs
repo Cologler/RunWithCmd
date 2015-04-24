85d9959b891ea41cc761e7b407996dc3fa1e8350
赖巍 <skyoflw@gmail.com>
2015-04-24 18:09:25 +0800

    1. create project

---
+++ RunWithCmd/RunWithCmd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunWithCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0 && System.IO.File.Exists(args[0]))
+            {
+                var folder = System.IO.Path.GetDirectoryName(args[0]);
+                System.IO.Directory.SetCurrentDirectory(folder);
+                System.Diagnostics.Process.Start("cmd.exe", "/c \"" + args[0] + "\"");
+            }
+        }
+    }
+}
